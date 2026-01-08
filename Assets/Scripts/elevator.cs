9403a2448d85b38f1e02aa916288a3932b6df07b
Salapille <noecool@outlook.fr>
2026-01-08 16:50:06 +0100

    debut ascenceur

--- Assets/Scripts/elevator.cs
+++ Assets/Scripts/elevator.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class elevator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float maxHight;
+    [SerializeField] private float lowerPoint;
+
+    private Transform blockSelf;
+
     void Start()
     {
-        
+        blockSelf = gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ToTop();
+        ToBottom();
     }
+
+    private void ToTop()
+    {
+        while (blockSelf.transform.position.y < maxHight)
+        {
+            blockSelf.transform.position = new Vector3(0, 1, 0) + blockSelf.transform.position;
+        }
+    }
+    private void ToBottom()
+    {
+        while (blockSelf.transform.position.y > lowerPoint)
+        {
+            blockSelf.transform.position = new Vector3(0, -1, 0) + blockSelf.transform.position;
+        }
+    }
+
 }
